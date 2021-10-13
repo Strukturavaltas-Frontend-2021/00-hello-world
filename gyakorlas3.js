4a41712666490928ca02388471fe207af289443f
Attila Hartmann <hartmann.attila88@gmail.com>
2021-10-13 10:50:10 +0200

    Exercise 3 & 4 completed

---
+++ gyakorlas3.js
@@ -0,0 +1,20 @@
+// 3. feladat: True vagy false a kifejezés értéke?
+console.log(2 == 2); // true
+
+console.log(0 == ""); // true
+
+console.log(null === undefined); // false
+
+console.log(0 == false); // true
+
+console.log(2 == '2'); // true
+
+console.log(2 == 22); // false
+
+console.log(null == undefined); // true
+
+console.log(0 === ""); // false
+
+console.log(0 === false); // false
+
+console.log(2 === '2'); // false
--- gyakorlas4.js
+++ gyakorlas4.js
@@ -1,18 +1,28 @@
 // 4. feladat: Mit ír ki a program a console.log(...) hívásoknál?
-console.log("This is not a joke " + "" + ", unfortunately.");
 
-console.log("This is not a joke " + 2 ** 3 + ", unfortunately.");
+// console.log("This is not a joke " + "" + ", unfortunately.");
+// This is not a joke , unfortunately.
 
-console.log("This is not a joke " + "2 ** 3" + ", unfortunately.");
+// console.log("This is not a joke " + 2 ** 3 + ", unfortunately.");
+// This is not a joke 8, unfortunately.
 
-console.log(7 * 2 + 4 + " " + "This is not a joke, unfortunately.");
+// console.log("This is not a joke " + "2 ** 3" + ", unfortunately.");
+// This is not a joke 2 ** 3, unfortunately.
 
-console.log("" + 18 + 9 ** 2);
+// console.log(7 * 2 + 4 + " " + "This is not a joke, unfortunately.");
+// 18 This is not a joke, unfortunately.
 
-console.log("" + 18 - 9 ** 2);
+// console.log("" + 18 + 9 ** 2);
+// 1881 (string)
 
-console.log(18 - 9 ** 2 + "");
+// console.log("" + 18 - 9 ** 2);
+// -63 (szám)
 
-console.log("Result: " + 18 + 9 ** 2);
+// console.log(18 - 9 ** 2 + "");
+// -63 (string)
+
+// console.log("Result: " + 18 + 9 ** 2);
+// Result: 1881 (string)
 
 console.log("Result: " + 18 - 9 ** 2);
+// NaN
