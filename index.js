58ccc3d4ebd96d06d0e0e3107b95ccae3308a2b4
Veréb Tamás <vereb.tamas@gmail.com>
2021-10-20 23:27:23 +0200

    Órai gyakorlás

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const myName = "Tamás";
-console.log(`Hello ${myName}!`);
+let a;
+let b = 1e6;
+
+// console.log(a);
+// console.log(b);
+
+a = b;
+b = b + 5;
+
+// console.log(`A = ${a}, B = ${b}`);
+
+const myString = "my string";
+console.log(("Here is " + myString).toUpperCase());
+console.log(`Here is ${myString}`);
+
+console.log(myString.substring(3));
+console.log(myString.substring(0,2)+myString.substring(3));
