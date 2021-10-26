9eab3e2c3d713d2a2b173329dd435fdc8d747b1a
theEqual <theEqual@github.com>
2021-10-26 21:57:34 +0200

    .

---
+++ operatorok.js
@@ -0,0 +1,12 @@
+// console.log('b' && false);
+
+console.log("This is not a joke " + "" + ", unfortunately.");
+console.log("This is not a joke " + 2 ** 3 + ", unfortunately.");
+console.log(7 * 2 + 4 + " " + "This is not a joke, unfortunately.");
+console.log("" + 18 + 9 ** 2);
+console.log("" + 18 - 9 ** 2);
+console.log(18 - 9 ** 2 + "");
+
+console.log("Result: " + 18 + 9 ** 2); //Result: 1881
+
+console.log("Result: " + 18 - 9 ** 2); // NaN
